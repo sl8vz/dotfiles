951e2302b5ee5b18109857879ebbfac6d562a852
sl8vz <slaveze@gmail.com>
2024-03-30 15:03:32 +0100

    nvim: disable indent lines by default
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/indent_line.lua
+++ nvim/.config/nvim/lua/plugins/indent_line.lua
@@ -1,9 +1,12 @@
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    config = function()
+      require('ibl').setup {
+        enabled = false,
+      }
+      vim.keymap.set('n', '<F4>', ':IBLToggle<cr>', { desc = 'Toggle indent line' })
+    end,
   },
 }
