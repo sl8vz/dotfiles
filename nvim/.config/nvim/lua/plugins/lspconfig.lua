5fedd9ed51895eb968bb263e5b3b25b25a794ce4
sl8vz <slaveze@gmail.com>
2024-05-28 09:11:53 +0200

    nvim: lspconfig: set default LSP servers
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -135,19 +135,13 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          cmd = { 'clangd', '--background-index', '--query-driver=/home/seb/toolchains/**/bin/arm-none-eabi-*'},
+        },
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        pyright = {},
+        rust_analyzer = {},
+        cmake = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
