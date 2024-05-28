48faa5c12272fcea5449d3dd4061178f646e4428
sl8vz <slaveze@gmail.com>
2024-05-28 09:11:18 +0200

    nvim: add gitsigns keymaps
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -6,15 +6,26 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    config = function()
+      require('gitsigns').setup {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      }
+
+      vim.keymap.set('n', '<leader>hn', ':Gitsigns next_hunk<cr>', { desc = 'Next git hunk' })
+      vim.keymap.set('n', '<leader>hp', ':Gitsigns prev_hunk<cr>', { desc = 'Previous git hunk' })
+      vim.keymap.set('n', '<leader>hu', ':Gitsigns reset_hunk<cr>', { desc = 'Reset git hunk' })
+      vim.keymap.set('n', '<leader>gR', ':Gitsigns reset_buffer<cr>', { desc = 'Reset git buffer' })
+      vim.keymap.set('n', '<leader>hs', ':Gitsigns stage_hunk<cr>', { desc = 'Stage git hunk' })
+      vim.keymap.set('n', '<leader>hS', ':Gitsigns undo_stage_hunk<cr>', { desc = 'Undo stage git hunk' })
+      vim.keymap.set('n', '<leader>hb', ':Gitsigns blame_line<cr>', { desc = 'Blame git line' })
+
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
