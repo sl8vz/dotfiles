091098f25d11f3a860af96ceab11b4728b0bb4f5
sl8vz <slaveze@gmail.com>
2024-05-28 09:10:27 +0200

    nvim: switch to nvim-tree
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/filetree.lua
+++ nvim/.config/nvim/lua/plugins/filetree.lua
@@ -1,15 +1,32 @@
 -- File: lua/custom/plugins/filetree.lua
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
+  "nvim-tree/nvim-tree.lua",
   version = "*",
+  lazy = false,
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
   },
-  config = function ()
-    require('neo-tree').setup {}
-    vim.keymap.set('n', '<F9>', ':Neotree toggle reveal right<cr>', { desc = 'Toggle file tree' })
+  config = function()
+    require("nvim-tree").setup {
+      disable_netrw = true,
+      hijack_netrw = true,
+      update_focused_file = {
+        enable = true,
+        update_cwd = false,
+        ignore_list = {},
+      },
+      system_open = {
+        cmd = nil,
+        args = {},
+      },
+      view = {
+        width = 30,
+        side = "right",
+      },
+      diagnostics = {
+        enable = true,
+      },
+    }
+    vim.keymap.set('n', '<F9>', ':NvimTreeToggle<cr>', { desc = 'Toggle file tree' })
   end,
 }
