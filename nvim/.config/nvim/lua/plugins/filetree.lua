e5ae8ab8c1bc593622926e36d578272471680af8
sl8vz <slaveze@gmail.com>
2025-02-19 13:06:57 +0100

    nvim: add filetree open in splits keymaps
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/filetree.lua
+++ nvim/.config/nvim/lua/plugins/filetree.lua
@@ -25,8 +25,24 @@ return {
       },
       diagnostics = {
         enable = true,
+        show_on_dirs = true,
       },
+      on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        --custom mappings
+        vim.keymap.set("n", "<leader>v", api.node.open.vertical,    opts("Open file in vertical split"))
+        vim.keymap.set("n", "<leader>s", api.node.open.horizontal,  opts("Open file in horizontal split"))
+      end,
     }
     vim.keymap.set('n', '<F9>', ':NvimTreeToggle<cr>', { desc = 'Toggle file tree' })
   end,
 }
+
