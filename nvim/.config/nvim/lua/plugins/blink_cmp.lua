413ac00f3b1c758ce641a413bac36761e11cdcd8
sl8vz <slaveze@gmail.com>
2025-07-30 15:02:44 +0200

    nvim: blink: add copilot integration
    Signed-off-by: sl8vz <slaveze@gmail.com>

--- nvim/.config/nvim/lua/plugins/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -1,7 +1,10 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'giuxtaposition/blink-cmp-copilot'
+  },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -43,7 +46,15 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
